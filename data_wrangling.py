ef0bc8ab42097d14c964c27995370620b0fec653
nghiemria <nghiem.hoangduong@gmail.com>
2021-10-17 15:29:50 +0200

    data wrangling

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,12 +1,15 @@
-# DATA ACQUISION
+## DATA ACQUISION
 
 # import pandas library
 import pandas as pd
 import numpy as np
+import matplotlib.pylab as plt
+import pylab
+from matplotlib import pyplot
 other_path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
 df = pd.read_csv(other_path, header=None)
 
-# ADD HEADER
+## ADD HEADER
 # create headers list
 headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
          "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
@@ -14,13 +17,88 @@ headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num
          "peak-rpm","city-mpg","highway-mpg","price"]
 df.columns = headers
 # replace ? with NaN
-df1=df.replace('?',np.NaN)
-# drop missing value from price column
-df=df1.dropna(subset=["price"], axis=0)
-print(df.head(5))
+df1=df.replace('?',np.NaN,inplace=True)
 
-# SAVE DATA SET
+
+## EVALUATING FOR MISSING DATA
+missing_data=df.isnull()
+#print(missing_data.head(5))
+# count missing values in each column
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print(missing_data[column].value_counts())
+
+
+## DEAL WITH MISSING DATA
+
+# replace missing data with mean
+avg_norm_loss = df["normalized-losses"].astype("float").mean(axis=0)
+df["normalized-losses"].replace(np.nan, avg_norm_loss, inplace=True)
+avg_bore=df['bore'].astype('float').mean(axis=0)
+df["bore"].replace(np.nan, avg_bore, inplace=True)
+avg_horsepower = df['horsepower'].astype('float').mean(axis=0)
+df['horsepower'].replace(np.nan, avg_horsepower, inplace=True)
+avg_peakrpm=df['peak-rpm'].astype('float').mean(axis=0)
+df['peak-rpm'].replace(np.nan, avg_peakrpm, inplace=True)
+
+#replace the missing 'num-of-doors' values by the most frequent
+df['num-of-doors'].value_counts()
+df["num-of-doors"].replace(np.nan, "four", inplace=True)
+
+# simply drop whole row with NaN in "price" column
+df.dropna(subset=["price"], axis=0, inplace=True)
+# reset index, because we droped two rows
+df.reset_index(drop=True, inplace=True)
+
+
+## CORRECT DATA TYPE
+df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
+df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
+df[["price"]] = df[["price"]].astype("float")
+df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+
+
+## DATA STANDARDISATION
+# Convert mpg to L/100km by mathematical operation (235 divided by mpg)
+df['city-L/100km'] = 235/df["city-mpg"]
+df["highway-L/100km"] = 235/df["highway-mpg"]
+
+# replace (original value) by (original value)/(maximum value)
+df['length'] = df['length']/df['length'].max()
+df['width'] = df['width']/df['width'].max()
+df["height"] = df["height"]/df["length"].max()
+
+## BINNING
+df["horsepower"]=df["horsepower"].astype(int, copy=True)
+bins = np.linspace(min(df["horsepower"]), max(df["horsepower"]), 4)
+group_names = ['Low', 'Medium', 'High']
+df['horsepower-binned'] = pd.cut(df['horsepower'], bins, labels=group_names, include_lowest=True )
+print(df[['horsepower','horsepower-binned']].head(20))
+
+import matplotlib as plt
+
+# draw historgram of attribute "horsepower" with bins = 3
+final_plot = plt.pyplot.hist(df["horsepower"], bins = 3)
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+pylab.savefig("D:/3 - Programming\data-analysis-python/binning_histogram.pdf")
+
+
+## DUMMY VARIABLE
+dummy_variable_1 = pd.get_dummies(df["fuel-type"])
+dummy_variable_1.rename(columns={'gas':'fuel-type-gas', 'diesel':'fuel-type-diesel'}, inplace=True)
+
+# merge data frame "df" and "dummy_variable_1"
+
+df = pd.concat([df, dummy_variable_1], axis=1)
+# drop original column "fuel-type" from "df"
+df.drop("fuel-type", axis = 1, inplace=True)
+
+## SAVE DATA SET
 df.to_csv("automobile.csv", index=False)
-print(df.dtypes)
-df.describe()
-df.describe(include='all')
+#print(df.dtypes)
+#df.describe()
+#df.describe(include='all')
